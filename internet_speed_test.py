0b0b1c6f82bcde6bfa32b3d7bbc191128ba8c4c9
Chris Farfan <138206825+ChrisFarfan-CN@users.noreply.github.com>
2024-01-09 01:24:54 +0200

    Got it working!

--- internet_speed_test.py
+++ internet_speed_test.py
@@ -1,8 +1,10 @@
-# pip install speedtest-cli pandas
+# pip install speedtest-cli
+# pip install pandas
+# pip install openpyxl
 import speedtest, datetime
 import pandas as pd
 
-duration = 300 # seconds
+duration = 10 # seconds
 results = []
 
 st = speedtest.Speedtest()
@@ -13,7 +15,7 @@ target_time = time_now + datetime.timedelta(0,duration)
 while datetime.datetime.now() <= target_time:
     down = st.download()
     up = st.upload()
-    results.append({"time":datetime.datetime.now(),"down":down,"up":up})
+    results.append({"yyyy mm dd hh:mm:ss":datetime.datetime.now(),"down":down,"up":up})
 
 df = pd.DataFrame(results)
 print(df.to_string())
