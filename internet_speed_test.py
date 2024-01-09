f5db7356d8b203bc45df6d63969d46215a7a5d64
Chris Farfan <138206825+ChrisFarfan-CN@users.noreply.github.com>
2024-01-09 02:06:25 +0200

    Finalised and added comments

--- internet_speed_test.py
+++ internet_speed_test.py
@@ -1,22 +1,28 @@
 # pip install speedtest-cli
 # pip install pandas
 # pip install openpyxl
-import speedtest, datetime
-import pandas as pd
 
-duration = 10 # seconds
-results = []
+import speedtest, datetime # import `speedtest` and `datetime`.
+import pandas as pd # import `pandas` as `pd` for dataframes and `to_excel()`.
 
-st = speedtest.Speedtest()
+duration = 600 #? {seconds} How long do you want to test for?
 
-time_now = datetime.datetime.now()
-target_time = time_now + datetime.timedelta(0,duration)
+st = speedtest.Speedtest() # Create the speedtest object.
+df = pd.DataFrame(columns = ["date-time","download (b/s)","upload (b/s)"]) # Make our DataFrame with columns.
 
-while datetime.datetime.now() <= target_time:
-    down = st.download()
-    up = st.upload()
-    results.append({"yyyy mm dd hh:mm:ss":datetime.datetime.now(),"down":down,"up":up})
+time_now = datetime.datetime.now() # Fetch the current time.
+target_time = time_now + datetime.timedelta(0,duration) # Calculate the time we should stop at.
 
-df = pd.DataFrame(results)
-print(df.to_string())
-df.to_excel("results.xlsx")
+while time_now <= target_time: # While we still have time to run:
+    down = st.download() # Do the download test.
+    up = st.upload() # Do the upload test.
+    df.loc[len(df.index)] = [
+        ((datetime.datetime.now()).strftime("%Y/%m/%d %H:%M")),
+        down,
+        up
+    ] # Creat a new entry containing the time and results.
+    time_now = datetime.datetime.now() # Check the time.
+
+print(df) # Print the results to the terminal.
+
+df.to_excel(f"results{time_now.year}-{time_now.month}-{time_now.day}-{time_now.hour}h{time_now.second}s.xlsx") # Export the results to a spreadsheet.
