2b83b7d314fb4dde4f9e923234223f29ea56a996
Chris Farfan <138206825+ChrisFarfan-CN@users.noreply.github.com>
2024-01-10 13:30:29 +0200

    fixed export bug?

--- internet_speed_test.py
+++ internet_speed_test.py
@@ -2,24 +2,27 @@
 # pip install pandas
 # pip install openpyxl
 
+from distutils.command import upload
 import speedtest, datetime # import `speedtest` and `datetime`.
 import pandas as pd # import `pandas` as `pd` for dataframes and `to_excel()`.
 
 duration = 1200 #? {seconds} How long do you want to test for?
 
 st = speedtest.Speedtest() # Create the speedtest object.
-df = pd.DataFrame(columns = ["date-time","download (b/s)","upload (b/s)"]) # Make our DataFrame with columns.
+df = pd.DataFrame(columns = ["Date-time","Download (b/s)","Upload (b/s)"]) # Make our DataFrame with columns.
 
 time_now = datetime.datetime.now() # Fetch the current time.
 target_time = time_now + datetime.timedelta(0,duration) # Calculate the time we should stop at.
 
 print("Starting tests:")
 while time_now <= target_time: # While we still have time to run:
-    print(f"> Estimate: {(target_time - time_now).seconds} seconds left\n> Download test started...") # Progress tracking
+    print(f"> Estimate: {(target_time - time_now).seconds} secs left\nDownload test started...") # Progress tracking
     down = st.download() # Do the download test.
+    print(f"> Download = {(down/1000000):.3g} Mb/s")
     time_now = datetime.datetime.now() # Check the time.
-    print(f"> Estimate: {(target_time - time_now).seconds} seconds left\n> Upload test started...") # Progress tracking
+    print(f"> Estimate: {(target_time - time_now).seconds} secs\nUpload test started...") # Progress tracking
     up = st.upload() # Do the upload test.
+    print(f"> Upload = {(up/1000000):.3g} Mb/s")
     df.loc[len(df.index)] = [
         ((datetime.datetime.now()).strftime("%Y/%m/%d %H:%M")),
         down,
@@ -29,4 +32,4 @@ while time_now <= target_time: # While we still have time to run:
 
 print(df) # Print the results to the terminal.
 
-df.to_excel(f"results{time_now.year}-{time_now.month}-{time_now.day}-{time_now.hour}:{time_now.second}.xlsx") # Export the results to a spreadsheet.
+df.to_excel(f"results{time_now.year}-{time_now.month}-{time_now.day}-{time_now.hour}{time_now.second}.xlsx") # Export the results to a spreadsheet.
