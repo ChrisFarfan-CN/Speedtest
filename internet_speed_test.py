72ec7683c89b03926c5eebd3cc528aefb423f8df
Chris Farfan <138206825+ChrisFarfan-CN@users.noreply.github.com>
2024-01-10 15:25:04 +0200

    b/s to Mb/s (bits that is, not bytes)

--- internet_speed_test.py
+++ internet_speed_test.py
@@ -9,7 +9,7 @@ import pandas as pd # import `pandas` as `pd` for dataframes and `to_excel()`.
 duration = 1200 #? {seconds} How long do you want to test for?
 
 st = speedtest.Speedtest() # Create the speedtest object.
-df = pd.DataFrame(columns = ["Date-time","Download (b/s)","Upload (b/s)"]) # Make our DataFrame with columns.
+df = pd.DataFrame(columns = ["Date-time","Download (Mb/s)","Upload (Mb/s)"]) # Make our DataFrame with columns.
 
 time_now = datetime.datetime.now() # Fetch the current time.
 target_time = time_now + datetime.timedelta(0,duration) # Calculate the time we should stop at.
@@ -17,12 +17,12 @@ target_time = time_now + datetime.timedelta(0,duration) # Calculate the time we 
 print("Starting tests:")
 while time_now <= target_time: # While we still have time to run:
     print(f"> Estimate: {(target_time - time_now).seconds} secs left\nDownload test started...") # Progress tracking
-    down = st.download() # Do the download test.
-    print(f"> Download = {(down/1000000):.3g} Mb/s")
+    down = st.download()/1000000 # Do the download test.
+    print(f"> Download = {(down):.3g} Mb/s")
     time_now = datetime.datetime.now() # Check the time.
     print(f"> Estimate: {(target_time - time_now).seconds} secs\nUpload test started...") # Progress tracking
-    up = st.upload() # Do the upload test.
-    print(f"> Upload = {(up/1000000):.3g} Mb/s")
+    up = st.upload()/1000000 # Do the upload test.
+    print(f"> Upload = {(up):.3g} Mb/s")
     df.loc[len(df.index)] = [
         ((datetime.datetime.now()).strftime("%Y/%m/%d %H:%M")),
         down,
